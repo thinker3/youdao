6dfce16a93a971b8f3ad38df34c901b597e034fd
thinker3 <metathinkerk@gmail.com>
2013-09-04 15:47:59 +0800

    text scrollbar grid layout

--- tkinter_version.py
+++ tkinter_version.py
@@ -19,43 +19,59 @@ class GUI(threading.Thread):
         threading.Thread.__init__(self)
         self.root = Tk()
         self.root.title("Press ctrl to search seleted word")
-        screen_width = self.root.winfo_screenwidth()
-        screen_height = self.root.winfo_screenheight()
-        self.root.geometry("550x250+%d+%d" % (screen_width/2-275, screen_height/2-125))
         self.root.protocol("WM_DELETE_WINDOW", self.close_handler)
+        self.frame=Frame(self.root)
         self.start()
         self.init_UI()
         self.root.mainloop()
 
+    def center(self):
+        w = self.root.winfo_screenwidth()
+        h = self.root.winfo_screenheight()
+        self.rootsize = tuple(int(_) for _ in self.root.geometry().split('+')[0].split('x'))
+        x = w/2 - self.rootsize[0]/2
+        y = h/2 - self.rootsize[1]/2
+        self.root.geometry("%dx%d+%d+%d" % (self.rootsize + (x, y)))
+
     def init_UI(self):
-        self.label_name = Label(self.root, text='')
-        self.label_name.pack()
-        self.label_phonetic = Label(self.root, text='')
-        self.label_phonetic.pack()
-        self.area_meaning = Label(self.root, text='')
-        self.area_meaning.pack()
+        self.root.iconify()
+        self.label_name = Label(self.frame, text='')
+        self.label_name.grid(row=0)
+        self.label_phonetic = Label(self.frame, text='')
+        self.label_phonetic.grid(row=1)
 
+        self.area_meaning =Text(self.frame,height=3,width=50,wrap=WORD)
+        self.area_meaning.grid(row=2)
+        scroll_meaning=Scrollbar(self.frame)
+        scroll_meaning.grid(row=2, column=1, sticky=N+S)
+        scroll_meaning.config(command=self.area_meaning.yview)
+        self.area_meaning.tag_config('color', background='red', foreground='white') # , wrap='word'
+        self.area_meaning.configure(yscrollcommand=scroll_meaning.set)
 
-        self.frame=Frame(self.root)
-        self.frame.pack()
+        self.area_example =Text(self.frame,height=7,width=50,background='white',wrap=WORD)
+        self.area_example.grid(row=3)
+        scroll_example=Scrollbar(self.frame)
+        scroll_example.grid(row=3, column=1, sticky=N+S)
+        scroll_example.config(command=self.area_example.yview)
+        self.area_example.tag_config(SEL, foreground='red')
+        self.area_example.configure(yscrollcommand=scroll_example.set)
 
-        self.area_example =Text(self.frame,height=10,width=50,background='white')
-        scroll=Scrollbar(self.frame)
-        self.area_example.configure(yscrollcommand=scroll.set)
-        self.area_example.pack(side=LEFT)
-        scroll.pack(side=RIGHT,fill=Y)
-        #self.frame.pack(side=TOP)
+        self.frame.pack(padx=5, pady=5)
 
     def show_in_gui(self):
+        self.center()
         self.label_name.__setitem__('text', self.item.name)
-        self.label_name.pack()
         self.label_phonetic.__setitem__('text', self.item.phonetic)
-        self.label_phonetic.pack()
-        self.area_meaning.__setitem__('text', self.item.meaning)
-        self.area_meaning.pack()
 
-        self.area_example.insert(INSERT, '')
+        self.area_meaning.config(state=NORMAL)
+        self.area_meaning.delete('1.0', END)
+        self.area_meaning.insert(INSERT, self.item.meaning, 'color')
+        self.area_meaning.config(state=DISABLED)
+
+        self.area_example.config(state=NORMAL)
+        self.area_example.delete('1.0', END)
         self.area_example.insert(INSERT, self.item.example)
+        self.area_example.config(state=DISABLED)
 
         self.root.deiconify()
         self.root.attributes('-topmost', 1)
