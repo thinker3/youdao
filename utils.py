f192493fa51a8522bc471dcf64269bfb7e4c0ca6
thinker3 <metathinkerk@gmail.com>
2014-03-17 16:44:08 +0800

    virtualenv, scrapy upgrade

--- utils.py
+++ utils.py
@@ -4,26 +4,33 @@ import os
 from datetime import datetime
 from models import XmlItem
 from lxml import etree
-from scrapy.selector import XmlXPathSelector
+#from scrapy.selector import XmlXPathSelector
+from scrapy.selector import Selector
 
 DB_FILE_NAME = 'webyoudao.db'
 XML_FILE_NAME = 'wordbook.myxml'
-DROPBOX_YOUDAO_PATH = '~/Dropbox/youdao_db_xml/' # endswith '/'
+DROPBOX_YOUDAO_PATH = '~/Dropbox/youdao_db_xml/'  # endswith '/'
+
 
 def abspath():
     dirname = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(dirname, XML_FILE_NAME), os.path.join(dirname, DB_FILE_NAME)
+    return (os.path.join(dirname, XML_FILE_NAME),
+            os.path.join(dirname, DB_FILE_NAME))
+
 
 def get_xml_db_path():
     if DROPBOX_YOUDAO_PATH:
-        xml_in_dropbox = os.path.expanduser(DROPBOX_YOUDAO_PATH) + XML_FILE_NAME 
-        db_in_dropbox = os.path.expanduser(DROPBOX_YOUDAO_PATH) + DB_FILE_NAME 
+        xml_in_dropbox = os.path.expanduser(
+            DROPBOX_YOUDAO_PATH) + XML_FILE_NAME
+        db_in_dropbox = os.path.expanduser(DROPBOX_YOUDAO_PATH) + DB_FILE_NAME
+        db_in_dropbox = os.path.expanduser(DROPBOX_YOUDAO_PATH) + DB_FILE_NAME
     if os.path.exists(xml_in_dropbox) and os.path.exists(db_in_dropbox):
         return xml_in_dropbox, db_in_dropbox
     else:
         return abspath()
 
 xmlpath, dbpath = get_xml_db_path()
+
 
 def init_list():
     temp = []
@@ -39,22 +46,23 @@ def init_list():
             pass
     if not xml:
         return temp
-    xxs =XmlXPathSelector(text=xml)
-    items = xxs.select('//wordbook/item')
+    xxs = Selector(text=xml)
+    items = xxs.xpath('//wordbook/item')
     for item in items:
-        name = item.select('./name/text()').extract()[0]
+        name = item.xpath('./name/text()').extract()[0]
         one = XmlItem(name)
-        create_time = item.select('./create_time/text()').extract()[0]
-        create_time= datetime.strptime(create_time, '%Y-%m-%d %H:%M:%S')
-        access_time = item.select('./access_time/text()').extract()[0]
+        create_time = item.xpath('./create_time/text()').extract()[0]
+        create_time = datetime.strptime(create_time, '%Y-%m-%d %H:%M:%S')
+        access_time = item.xpath('./access_time/text()').extract()[0]
         access_time = datetime.strptime(access_time, '%Y-%m-%d %H:%M:%S')
-        score = item.select('./score/text()').extract()[0]
+        score = item.xpath('./score/text()').extract()[0]
         score = int(score)
         setattr(one, 'create_time', create_time)
         setattr(one, 'access_time', access_time)
         setattr(one, 'score', score)
         temp.append(one)
     return temp
+
 
 def save_list(items):
     wordbook = etree.Element('wordbook')
@@ -75,8 +83,8 @@ def save_list(items):
                 item.append(create_time)
                 item.append(access_time)
         wordbook.append(item)
-    s = etree.tostring(wordbook, pretty_print=True, encoding='utf8') # encoding is important
+    # encoding is important
+    s = etree.tostring(wordbook, pretty_print=True, encoding='utf8')
     f = open(xmlpath, 'w')
     f.write(s)
     f.close()
-
--- youdao.py
+++ youdao.py
@@ -1,10 +1,11 @@
 #coding=utf8
 
-import os, re, urllib2, urlgrabber, requests
+import os
 from datetime import datetime
-from scrapy.selector import HtmlXPathSelector
+#from scrapy.selector import HtmlXPathSelector
+from scrapy.selector import Selector
 from splinter import Browser
-import subprocess
+
 
 class Fetcher(object):
     def __init__(self):
@@ -16,21 +17,25 @@ class Fetcher(object):
 
     def get_html_by_phantomjs(self, url):
         self.browser.visit(url)
-        html = self.browser.html # <type 'unicode'>
+        html = self.browser.html  # <type 'unicode'>
         html = html.encode('utf-8')
         return html
 
     def get_html_by_urllib2(self, url):
+        import urllib2
         html = urllib2.urlopen(url).read()
         return html
 
     def get_html_by_urlgrabber(self, url):
+        import urlgrabber
         html = urlgrabber.urlopen(url).read()
         return html
 
     def get_html_by_wget(self, url):
+        #import subprocess
         #subprocess.call(["wget", "-r", "-np", "-A", "files", url])
-        cmd = 'wget -q -O tempword.html %s' % url # q, quiet; big O, output-document
+        # q, quiet; big O, output-document
+        cmd = 'wget -q -O tempword.html %s' % url
         cmd = cmd.replace('&', '\&')
         os.system(cmd)
         f = open('tempword.html', 'r')
@@ -40,45 +45,48 @@ class Fetcher(object):
         return html
 
     def get_html_by_requests(self, url):
+        import requests
         html = requests.get(url).text
         return html
 
     def get_html(self, url):
-        return self.get_html_by_requests(url) # 1
-        #return self.get_html_by_urlgrabber(url) # 2
-        #return self.get_html_by_wget(url) # 2
+        return self.get_html_by_requests(url)  # 1
+        #return self.get_html_by_urlgrabber(url)  # 2
+        #return self.get_html_by_wget(url)  # 2
         if self.which_getter == 'phantomjs':
             try:
-                self.browser = Browser('phantomjs') # 3
+                self.browser = Browser('phantomjs')  # 3
                 return self.get_html_by_phantomjs(url)
             except:
-                return self.get_html_by_urllib2(url) # 2
+                return self.get_html_by_urllib2(url)  # 2
         else:
             return self.get_html_by_urllib2(url)
 
     def query(self, word):
-        url = "http://dict.youdao.com/search?tab=chn&keyfrom=dict.top&q=" + word
+        url = "http://dict.youdao.com/search?tab=chn&keyfrom=dict.top&q="
+        url += word
         before_fetching = datetime.now()
         html = self.get_html(url)
         after_fetching = datetime.now()
         time_fetching = after_fetching - before_fetching
         print 'time_fetching %f' % time_fetching.total_seconds()
-        hxs = HtmlXPathSelector(text=html)
+        hxs = Selector(text=html)
         time_parsing = datetime.now() - after_fetching
         print 'time_parsing %f' % time_parsing.total_seconds()
 
-        phonetics = hxs.select('//div[@id="phrsListTab"]/h2[1]/div[1]/span')
+        phonetics = hxs.xpath('//div[@id="phrsListTab"]/h2[1]/div[1]/span')
         phonetic = ''
         for ph in phonetics:
-            ph = ph.select('./span/text()').extract()
+            ph = ph.xpath('./span/text()').extract()
             if len(ph) > 0:
                 phonetic += ph[0]
 
-        lis = hxs.select('//div[@id="phrsListTab"]/div[@class="trans-container"]/ul/li')
+        lis = hxs.xpath(
+            '//div[@id="phrsListTab"]/div[@class="trans-container"]/ul/li')
         meaning = ''
         for li in lis:
             try:
-                temp_meaning = li.select('./text()').extract()[0] + '\n'
+                temp_meaning = li.xpath('./text()').extract()[0] + '\n'
             except:
                 temp_meaning = ''
             if word.capitalize() in temp_meaning and u'人名' in temp_meaning:
@@ -86,33 +94,30 @@ class Fetcher(object):
             else:
                 meaning += temp_meaning
 
-        examples = hxs.select('//div[@id="bilingual"]/ul/li')
+        examples = hxs.xpath('//div[@id="bilingual"]/ul/li')
         example = ''
         for li in examples:
-            example_en = li.select('./p[1]/span//text()').extract()
-            example_cn = li.select('./p[2]/span//text()').extract()
+            example_en = li.xpath('./p[1]/span//text()').extract()
+            example_cn = li.xpath('./p[2]/span//text()').extract()
             example += ''.join(example_en) + '\n'
             example += ''.join(example_cn) + '\n'
 
         if not meaning:
             print 'wrong word ?'
             try:
-                possible = hxs.select('//div[@class="error-typo"]/h4/text()').extract()[0].strip() + '\n'
-                possible += hxs.select('//div[@class="error-typo"]//a/text()').extract()[0]
+                possible = hxs.xpath(
+                    '//div[@class="error-typo"]/h4/text()'
+                ).extract()[0].strip() + '\n'
+                possible += hxs.xpath(
+                    '//div[@class="error-typo"]//a/text()').extract()[0]
             except:
                 possible = ''
             return possible, False
         item_dict = {}
         item_dict['name'] = word
         item_dict['phonetic'] = phonetic
-        item_dict['meaning'] = meaning 
-        item_dict['example'] = example 
+        item_dict['meaning'] = meaning
+        item_dict['example'] = example
         return item_dict, True
 
 query_web = Fetcher()
-
-
-
-
-
-
