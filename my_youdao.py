365eb60f1650d3f69278b77ffd5cb24a43708281
thinker3 <metathinkerk@gmail.com>
2014-04-27 10:24:50 +0800

    flash

--- my_youdao.py
+++ my_youdao.py
@@ -1,14 +1,15 @@
-#coding=utf8
-import os, re, threading
+# coding=utf8
+import os
+import re
+import threading
 from time import sleep
-import peewee
-from Tkinter import Tk, Label, Toplevel, Frame
-from Tkinter import *
+from Tkinter import Tk, Label, Frame, Button, StringVar, Entry, END, INSERT
+from Tkinter import DISABLED, Text, WORD, Scrollbar, N, S, SEL, NORMAL
 
 from youdao import query_web
 from keylogger import fetch_keys
 from models import Item
-from recite import Recite
+from recite import Recite, Flash
 from utils import init_list, save_list
 
 
@@ -22,9 +23,10 @@ class GUI(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.root = Tk()
-        self.root.title("Press left ctrl and left shift to search selected word")
+        self.root.title(
+            "Press left ctrl and left shift to search selected word")
         self.root.protocol("WM_DELETE_WINDOW", self.close_handler)
-        self.frame=Frame(self.root)
+        self.frame = Frame(self.root)
         self.words = init_list()
         self.start()
         self.init_UI()
@@ -33,9 +35,10 @@ class GUI(threading.Thread):
     def center(self):
         w = self.root.winfo_screenwidth()
         h = self.root.winfo_screenheight()
-        self.rootsize = tuple(int(_) for _ in self.root.geometry().split('+')[0].split('x'))
-        x = w/2 - self.rootsize[0]/2
-        y = h/2 - self.rootsize[1]/2
+        self.rootsize = tuple(
+            int(_) for _ in self.root.geometry().split('+')[0].split('x'))
+        x = w / 2 - self.rootsize[0] / 2
+        y = h / 2 - self.rootsize[1] / 2
         self.root.geometry("%dx%d+%d+%d" % (self.rootsize + (x, y)))
 
     def init_UI(self):
@@ -43,43 +46,74 @@ class GUI(threading.Thread):
         self.name_string = StringVar()
         self.entry_name = Entry(self.frame, textvariable=self.name_string)
         self.entry_name.focus()
-        self.entry_name.grid(row=0)
-        self.entry_name.bind('<Return>', self.enter_handler) # '<Enter>' is fired when mouse entering the input box
-        self.entry_name.bind('<KP_Enter>', self.enter_handler) # for the numeric pad enter key
+        # '<Enter>' is fired when mouse entering the input box
+        self.entry_name.bind('<Return>', self.enter_handler)
+        # for the numeric pad enter key
+        self.entry_name.bind('<KP_Enter>', self.enter_handler)
 
-        self.btn_add = Button(self.frame, text="Add", command=self.add_to_xml, state=DISABLED)
-        self.btn_add.grid(row=0, column=1)
+        self.btn_add = Button(self.frame, text="Add",
+                              command=self.add_to_xml, state=DISABLED)
 
-        self.btn_recite = Button(self.frame, text="Recite", command=self.create_recite_window)
-        self.btn_recite.grid(row=1, column=1)
+        self.btn_recite = Button(
+            self.frame, text="Recite", command=self.create_recite_window)
         if not self.words:
             self.btn_recite.config(state=DISABLED)
 
+        self.btn_flash = Button(
+            self.frame, text="Flash", command=self.create_flash_window)
+        if not self.words:
+            self.btn_flash.config(state=DISABLED)
+
         self.label_phonetic = Label(self.frame, text='')
-        self.label_phonetic.grid(row=1)
 
-        self.area_meaning =Text(self.frame,height=5,width=90,wrap=WORD)
-        self.area_meaning.grid(row=2)
-        scroll_meaning=Scrollbar(self.frame)
-        scroll_meaning.grid(row=2, column=1, sticky=N+S)
-        scroll_meaning.config(command=self.area_meaning.yview)
+        self.area_meaning = Text(self.frame, height=5, width=90, wrap=WORD)
+        self.scroll_meaning = Scrollbar(self.frame)
+        self.scroll_meaning.config(command=self.area_meaning.yview)
         self.area_meaning.tag_config(SEL, background='red')
-        self.area_meaning.configure(yscrollcommand=scroll_meaning.set)
+        self.area_meaning.configure(yscrollcommand=self.scroll_meaning.set)
 
-        self.area_example =Text(self.frame,height=9,width=90,background='white',wrap=WORD)
-        self.area_example.grid(row=3)
-        scroll_example=Scrollbar(self.frame)
-        scroll_example.grid(row=3, column=1, sticky=N+S)
-        scroll_example.config(command=self.area_example.yview)
+        self.area_example = Text(
+            self.frame, height=9, width=90, background='white', wrap=WORD)
+        self.scroll_example = Scrollbar(self.frame)
+        self.scroll_example.config(command=self.area_example.yview)
         self.area_example.tag_config(SEL, foreground='red')
-        self.area_example.configure(yscrollcommand=scroll_example.set)
+        self.area_example.configure(yscrollcommand=self.scroll_example.set)
 
-        self.btn_save = Button(self.frame, text="Save", command=self.save_after_edit)
-        self.btn_save.grid(row=4)
+        self.btn_save = Button(
+            self.frame, text="Save", command=self.save_after_edit)
 
         self.btn_sort = Button(self.frame, text="Sort", command=self.sort)
-        self.btn_sort.grid(row=4, column=1)
+        self.lay_out()
 
+    def lay_out(self):
+        column = 0
+        row = 0
+        self.entry_name.grid(row=row, column=column)
+        row += 1
+        self.label_phonetic.grid(row=row, column=column)
+        row += 1
+        self.area_meaning.grid(row=row, column=column)
+        row += 1
+        self.area_example.grid(row=row, column=column)
+        row += 1
+        self.btn_save.grid(row=row, column=column)
+
+        column += 1
+        row = 2
+        self.scroll_meaning.grid(row=row, column=column, sticky=N + S)
+        row += 1
+        self.scroll_example.grid(row=row, column=column, sticky=N + S)
+
+        column += 1
+        row = 0
+        self.btn_add.grid(row=row, column=column)
+        row += 1
+        self.btn_recite.grid(row=row, column=column)
+        row += 1
+        self.btn_flash.grid(row=row, column=column)
+        row += 1
+        self.btn_sort.grid(row=row, column=column)
+        row += 1
         self.frame.pack(padx=5, pady=5)
 
     def sort(self):
@@ -98,7 +132,13 @@ class GUI(threading.Thread):
     def create_recite_window(self):
         self.app = Recite(self)
         self.btn_recite.config(state=DISABLED)
-    
+        self.btn_flash.config(state=DISABLED)
+
+    def create_flash_window(self):
+        self.app = Flash(self)
+        self.btn_recite.config(state=DISABLED)
+        self.btn_flash.config(state=DISABLED)
+
     def enter_handler(self, event):
         word = self.name_string.get().strip()
         self.search(word)
@@ -106,8 +146,10 @@ class GUI(threading.Thread):
     def btn_recite_handler(self):
         if self.words:
             self.btn_recite.config(state=NORMAL)
+            self.btn_flash.config(state=NORMAL)
         else:
             self.btn_recite.config(state=DISABLED)
+            self.btn_flash.config(state=DISABLED)
 
     def highlight(self):
         start = 1.0
@@ -120,8 +162,10 @@ class GUI(threading.Thread):
             end = '%s + %dc' % (pos, length)
             self.area_example.tag_add('highlight', pos, end)
             start = end
-            pos = self.area_example.search(self.item.name, start, stopindex=END)
-        self.area_example.tag_config('highlight', background='white', foreground='red')
+            pos = self.area_example.search(
+                self.item.name, start, stopindex=END)
+        self.area_example.tag_config('highlight', background='white',
+                                     foreground='red')
 
     def show_in_gui(self):
         self.center()
@@ -143,7 +187,7 @@ class GUI(threading.Thread):
         self.root.attributes('-topmost', 0)
         self.root.focus_force()
         self.entry_name.focus()
-        self.entry_name.select_range(0, END) # TclError: bad entry index "1.0"
+        self.entry_name.select_range(0, END)  # TclError: bad entry index "1.0"
         self.entry_name.icursor(END)
 
     def clear(self, word='', meaning=''):
@@ -163,7 +207,7 @@ class GUI(threading.Thread):
         self.root.focus_force()
 
     def in_xml(self):
-        #return self.item.name in [one.name for one in self.words]
+        # return self.item.name in [one.name for one in self.words]
         for one in self.words:
             if self.item.name == one.name:
                 return True
@@ -171,7 +215,7 @@ class GUI(threading.Thread):
 
     def add_to_xml(self):
         self.btn_add.config(state=DISABLED)
-        if self.in_xml(): # strange duplicate items
+        if self.in_xml():  # strange duplicate items
             return
         # if add a word that is not in db now to xml, save it to db.
         try:
@@ -179,7 +223,7 @@ class GUI(threading.Thread):
         except:
             item = None
         if not item:
-            self.item.save(force_insert=True) # only save() not save to db
+            self.item.save(force_insert=True)  # only save() not save to db
 
         try:
             if self.words:
@@ -205,14 +249,15 @@ class GUI(threading.Thread):
         self.item = item
 
     def save(self, item_dict):
-        if item_dict['example']: # if no example, do not save to db, but if added to xml, save it to db.
+        # if no example, do not save to db, but if added to xml, save it to db.
+        if item_dict['example']:
             self.item = Item.create(**item_dict)
         else:
             self.item = Item(**item_dict)
 
     def search(self, word):
         word = word.lower()
-        self.query_db(word) # if word not in db, set self.item = None
+        self.query_db(word)  # if word not in db, set self.item = None
         if not self.item:
             item_dict_or_str = ''
             success = False
@@ -221,11 +266,11 @@ class GUI(threading.Thread):
             except:
                 print 'web failure'
             if item_dict_or_str and success:
-                self.save(item_dict_or_str) # save and set self.item
+                self.save(item_dict_or_str)  # save and set self.item
         if self.item:
             self.show_in_gui()
         else:
-            self.clear(word, item_dict_or_str) # here it is a string
+            self.clear(word, item_dict_or_str)  # here it is a string
 
     def respond(self, modifiers):
         if modifiers['left ctrl'] and modifiers['left shift']:
--- recite.py
+++ recite.py
@@ -1,10 +1,15 @@
 #coding=utf8
 import random
 from utils import save_list
-from Tkinter import *
+from Tkinter import N, S, W
+from Tkinter import Toplevel, Frame, Entry, StringVar, Button, Label
+from Tkinter import Text, Scrollbar, WORD
+from Tkinter import DISABLED, NORMAL, END, INSERT
 from tkMessageBox import showinfo
 
+
 class Recite(object):
+
     def __init__(self, master):
         self.master = master
         self.window = Toplevel(self.master.root)
@@ -25,7 +30,8 @@ class Recite(object):
         self.entry_name = Entry(self.frame, textvariable=self.name_string)
         self.entry_name.grid(row=0, sticky=W)
         self.entry_name.bind('<Return>', self.enter_handler)
-        self.entry_name.bind('<KP_Enter>', self.enter_handler) # for the numeric pad enter key, no effects on message box
+        # for the numeric pad enter key, no effects on message box
+        self.entry_name.bind('<KP_Enter>', self.enter_handler)
 
         self.btn_del = Button(self.frame, text="Delete", command=self.delete)
         self.btn_del.grid(row=0, column=1)
@@ -33,13 +39,15 @@ class Recite(object):
         self.label_phonetic = Label(self.frame, text='')
         self.label_phonetic.grid(row=1, sticky=W)
 
-        self.btn_show_phonetic = Button(self.frame, text="Show phonetic", command=self.show_phonetic)
+        self.btn_show_phonetic = Button(self.frame,
+                                        text="Show phonetic",
+                                        command=self.show_phonetic)
         self.btn_show_phonetic.grid(row=1, column=1)
 
-        self.area_meaning =Text(self.frame,height=5,width=80,wrap=WORD)
+        self.area_meaning = Text(self.frame, height=5, width=80, wrap=WORD)
         self.area_meaning.grid(row=2)
-        scroll_meaning=Scrollbar(self.frame)
-        scroll_meaning.grid(row=2, column=1, sticky=N+S)
+        scroll_meaning = Scrollbar(self.frame)
+        scroll_meaning.grid(row=2, column=1, sticky=N + S)
         scroll_meaning.config(command=self.area_meaning.yview)
         self.area_meaning.configure(yscrollcommand=scroll_meaning.set)
 
@@ -60,7 +68,11 @@ class Recite(object):
         else:
             self.item.score -= 2
             self.rearrange(0)
-            showinfo('Result', "Sorry, wrong!\nThe answer is ( %s )!" % self.item.name, parent=self.frame)
+            showinfo(
+                'Result',
+                "Sorry, wrong!\nThe answer is ( %s )!" % self.item.name,
+                parent=self.frame,
+            )
         self.run()
 
     def show_phonetic(self):
@@ -84,10 +96,10 @@ class Recite(object):
             self.run()
 
     def rearrange(self, right):
-        if self.item != self.words[0]: # to avoid multiple enter events
+        if self.item != self.words[0]:  # to avoid multiple enter events
             return
         self.words.pop(0)
-        if self.item.score >= 6: # remove the word
+        if self.item.score >= 6:  # remove the word
             return
         if len(self.words) < 7:
             self.words.append(self.item)
@@ -113,16 +125,96 @@ class Recite(object):
             self.choose()
 
     def choose(self):
-        index = random.randint(len(self.words)/2, len(self.words)-1)
+        index = random.randint(len(self.words) / 2, len(self.words) - 1)
         choosed = self.words.pop(index)
-        print 'choosing %s, length is %d, index is %d, score is %d' % (choosed.name, len(self.words), index, choosed.score)
+        print 'choosing %s, length is %d, index is %d, score is %d' % (
+            choosed.name, len(self.words), index, choosed.score)
         self.words.insert(0, choosed)
 
 
+class Flash(Recite):
 
+    def __init__(self, master):
+        super(Flash, self).__init__(master)
+        self.window.title('Flashing')
 
+    def init_UI(self):
+        self.label_name = Label(self.frame, text='')
+        self.label_phonetic = Label(self.frame, text='')
+        self.btn_show_phonetic = Button(
+            self.frame,
+            text="Show phonetic",
+            command=self.show_phonetic)
 
+        self.btn_yes = Button(self.frame, text="Yes", command=self.yes)
+        self.btn_no = Button(self.frame, text="No", command=self.no)
+        self.btn_del = Button(self.frame, text="Delete", command=self.delete)
 
+        self.area_meaning = Text(self.frame, height=10, width=80, wrap=WORD)
+        self.scroll_meaning = Scrollbar(self.frame)
+        self.scroll_meaning.config(command=self.area_meaning.yview)
+        self.area_meaning.configure(yscrollcommand=self.scroll_meaning.set)
+        self.btn_show_meaning = Button(
+            self.frame,
+            text="Show meaning",
+            command=self.show_meaning)
+        self.lay_out()
 
+    def lay_out(self):
+        row = 0
+        column = 0
+        self.label_name.grid(row=row, sticky=W)
+        column += 1
+        self.btn_yes.grid(row=row, column=column)
+        column += 1
+        self.btn_del.grid(row=row, column=column)
 
+        row += 1
+        column = 0
+        self.label_phonetic.grid(row=row, column=column, sticky=W)
+        column += 1
+        self.btn_no.grid(row=row, column=column)
+        column += 1
+        self.btn_show_phonetic.grid(row=row, column=column)
 
+        row += 1
+        self.area_meaning.grid(row=row, sticky=W)
+        column = 1
+        self.scroll_meaning.grid(row=row, column=column, sticky=N + S)
+        column += 1
+        self.btn_show_meaning.grid(row=row, column=column)
+        self.frame.pack(padx=5, pady=5)
+
+    def run(self):
+        if len(self.words) == 0:
+            self.close_handler()
+            return
+        self.item = self.words[0]
+        self.label_name.config(text=self.item.name)
+        self.label_phonetic.config(text='')
+        self.btn_show_phonetic.config(state=NORMAL)
+        self.area_meaning.delete('1.0', END)
+
+    def yes(self, right=True):
+        self.show_phonetic()
+        self.show_meaning()
+        self.item.update_access_time()
+        showinfo('Result', str(right), parent=self.frame)
+        if right:
+            self.item.score += 1
+            self.rearrange(1)
+        else:
+            self.item.score -= 2
+            self.rearrange(0)
+        self.run()
+
+    def no(self):
+        self.yes(right=False)
+
+    def show_meaning(self):
+        self.area_meaning.delete('1.0', END)
+        try:
+            self.area_meaning.insert(INSERT, self.item.convert().meaning)
+        except:
+            self.words.pop(0)
+            self.run()
