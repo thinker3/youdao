e8ec7e488d9827851a27befc194020362d1ad3ba
thinker3 <metathinkerk@gmail.com>
2014-09-24 06:50:09 +0800

    quick_edit_mode.py

---
+++ quick_edit_mode.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys
+import ctypes
+
+
+win32 = ctypes.windll.kernel32
+handle = win32.GetStdHandle(-10) # 3 ?
+mode = ctypes.c_int(0)
+win32.GetConsoleMode(handle, ctypes.byref(mode))
+old_mode = mode.value
+
+
+def main():
+    # disable or enable Windows console(cmd.exe) quick edit mode
+    if 'on' in sys.argv:
+        new_mode = 231
+        win32.SetConsoleMode(handle, new_mode)
+    elif 'off' in sys.argv:
+        #new_mode = 167
+        new_mode = old_mode & (~0x0040)
+        win32.SetConsoleMode(handle, new_mode)
+    else:
+        print 'usage: python %s on(off)' % __file__
+
+main()
